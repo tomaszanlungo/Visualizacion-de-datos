c5d4f1d07ea31aad0c8f43b578887cd4a5d76c7c
tomaszanlungo <tomaszanlungo@gmail.com>
2023-04-04 09:00:26 -0300

    grafico 2
    archivo completo del grafico 2, incluido la imagen .jpg (backgruond), css, js y html

--- Grafico2/script.js
+++ Grafico2/script.js
@@ -3,13 +3,15 @@ import addTooltips from "../Grafico2/addTooltips.js";
 d3.csv('astronautas.csv', d3.autoType).then(data => {
   console.log(data)
 
-  let datosFiltrados = data.filter(d => d.eva_mision_hs !== 0 && d.ocupacion !== "participante de vuelo espacial")
+  let datosFiltrados = data.filter(d => d.eva_mision_hs !== 0 && 
+  d.ocupacion !== "participante de vuelo espacial" &&
+  d.ocupacion !== "piloto")
   console.log(datosFiltrados)
 
   let chart = Plot.plot({
     marks: [
       Plot.dot(datosFiltrados, {
-        x: 'edad_mision', //como filtrar los x=0
+        x: 'edad_mision', 
         y: 'eva_mision_hs',
         fill: 'ocupacion',
         fillOpacity: 0.6,
@@ -19,16 +21,29 @@ d3.csv('astronautas.csv', d3.autoType).then(data => {
     ],
     grid: true,
     nice: true,
-    zero: true,
-    width: 800,
-    height: 300,
+    width: 700,
+    height: 400,
     
     facet: {
       data: datosFiltrados,
       x: 'ocupacion',
     },
     
-    x: { ticks: 4 },
+    y:{
+      label: "hs eva mision",
+    },
+
+    x: { 
+      domain: [30,65],
+      ticks: 4,
+      label: "edad",
+    },
+
+    style: {
+      fontFamily: 'sans-serif',
+      fontSize: 11,
+    },
+
   })
   d3.select('#chart').append(() => chart)
   addTooltips(chart);
